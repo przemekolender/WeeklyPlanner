2465d681884c6543248184e0d658f7bbae9b2a87
przemekolender <olenderprzemek@gmail.com>
2022-01-27 23:08:04 +0100

    fix static

--- WeeklyPlanner/settings.py
+++ WeeklyPlanner/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'WeeklyPlanner.urls'
@@ -117,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+MEDIA_STATIC = os.path.join(BASE_DIR, 'static/media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
